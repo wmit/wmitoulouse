cda74dccce20d4e710e13749be0b6c453022ac46
arnaud sellenet <arnodmental@gmail.com>
2012-08-16 11:17:35 +0200

    Generating wmit/events controller

---
+++ app/controllers/wmit/events_controller.rb
@@ -0,0 +1,16 @@
+class Wmit::EventsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ spec/helpers/wmit/events_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Wmit::EventsHelper. For example:
+#
+# describe Wmit::EventsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe Wmit::EventsHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
