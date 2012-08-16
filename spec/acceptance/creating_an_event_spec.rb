f55833308f35977657c4ade8ec0f58fff37b846d
arnaud sellenet <arnodmental@gmail.com>
2012-08-16 17:23:02 +0200

    first acceptance test for adding an event

--- spec/acceptance/creating_an_event_spec.rb
+++ spec/acceptance/creating_an_event_spec.rb
@@ -1,4 +1,20 @@
 require File.dirname(__FILE__) + '/acceptance_helper'
 
 feature "Creating an event" do
+  let(:place) { FactoryGirl.create(:wmit_place) }
+
+  background do
+    place
+  end
+
+  scenario "From the home page, any user can click a link to create an event" do
+    visit '/'
+    click_link I18n.t('events.new.link')
+    within '#new_event_form' do
+      fill_in 'event[title]', with: 'A new event'
+      fill_in 'event[scheduled_at]', with: (Time.current + 1.day).to_s
+      choose place.to_s from: 'event[place_id]'
+      click_submit
+    end
+  end
 end
