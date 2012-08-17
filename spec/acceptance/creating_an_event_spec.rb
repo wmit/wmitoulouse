a4052aee692f3a08d8e6b70aff107bf7ffe9a6b6
arnaud sellenet <arnodmental@gmail.com>
2012-08-17 16:05:33 +0200

    update locale

--- spec/acceptance/creating_an_event_spec.rb
+++ spec/acceptance/creating_an_event_spec.rb
@@ -9,7 +9,7 @@ feature "Creating an event" do
 
   scenario "From the home page, any user can click a link to create an event (with an existing place)" do
     visit '/'
-    click_link I18n.t('events.new.link')
+    click_link I18n.t('wmit.events.new.link')
     within '#new_wmit_event' do
       fill_in 'wmit_event[title]', with: 'A new event'
       fill_in 'wmit_event[scheduled_at]', with: (Time.current + 1.day).to_s
